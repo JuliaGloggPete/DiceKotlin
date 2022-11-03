8bfae88137a5e31f0289b56766f73227d410e0f8
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2022-11-03 10:37:56 +0100

    took in users input and Imageview

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     var diceResult : Int = 0
-    var sides = 6
+    //var sidesByDefault = 6
 
     lateinit var resultNumber : TextView
+    lateinit var inputDiceSides : EditText
+    lateinit var Image : ImageView
 
 // prints new sequence every time
 
@@ -22,6 +27,9 @@ class MainActivity : AppCompatActivity() {
 
         var button = findViewById<Button>(R.id.rollButton)
         resultNumber = findViewById(R.id.showDice)
+        inputDiceSides = findViewById(R.id.editSide)
+        Image = findViewById(R.id.imageView)
+
 
 
         button.setOnClickListener {
@@ -29,19 +37,44 @@ class MainActivity : AppCompatActivity() {
 
 
             roll()
-            Log.d("###", "hallo $diceResult")
-            resultNumber.text =diceResult.toString()
+
+
 
         }
     }
 
-    fun roll(): Int{
+    fun roll(){
 
+        val sides =inputDiceSides.text.toString().toIntOrNull()
+
+if (sides == null){return}
 
        this.diceResult =  (1..sides).random()
 
 
-        return diceResult
+        resultNumber.text =diceResult.toString()
+        if (diceResult.toString().toInt()==1){
+            Image.setBackgroundResource(R.drawable.dice1)
+        }
+        else if (diceResult.toString().toInt()==2){
+            Image.setBackgroundResource(R.drawable.dice2)
+        }
+       else if(diceResult.toString().toInt()==3){
+            Image.setBackgroundResource(R.drawable.dice3)
+        }
+        else if(diceResult.toString().toInt()==4){
+            Image.setBackgroundResource(R.drawable.dice4)
+        }
+        else if(diceResult.toString().toInt()==5){
+            Image.setBackgroundResource(R.drawable.dice5)
+        }
+        else if(diceResult.toString().toInt()==6){
+            Image.setBackgroundResource(R.drawable.dice6)
+        }
+        else{Image.setBackgroundResource(R.drawable.dicemore)}
+
+
+
 
     }
 
